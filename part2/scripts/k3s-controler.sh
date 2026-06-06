a875a2f3eee3fdcd139153bc7a85db7129bcfec6
Blankx <briceguichou.dev@gmail.com>
2026-06-06 22:38:17 +0200

    ADD : Enhance k3s control script to wait for nodes and apply configuration

--- part2/scripts/k3s-controler.sh
+++ part2/scripts/k3s-controler.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --tls-san=192.168.56.110 --write-kubeconfig-mode=644" K3S_TOKEN=mytoken123 sh -
+
+until kubectl get nodes > /dev/null 2>&1; do
+    echo "Waiting for K3s to be ready..."
+    sleep 5
+done
+
+kubectl apply -R -f /vagrant/config/
